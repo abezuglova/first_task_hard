554695b5e768d22cff54dbbc1c3744a8ec74d6a7
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-03-23 17:18:59 +0300

    timetable widget created

---
+++ lib/widgets/timetable_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TimeTableWidget extends StatelessWidget {
+  const TimeTableWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Здесь будет расписание'),
+      ),
+    );
+  }
+}
