4b784ca8422fffbf572eb95604fb633963944de8
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-03-23 11:23:39 +0300

    tile widgets created

---
+++ lib/widgets/crystal_balance_tile_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CrystalBalanceTileWidget extends StatelessWidget {
+  const CrystalBalanceTileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/level_tile_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LevelTileWidget extends StatelessWidget {
+  const LevelTileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
