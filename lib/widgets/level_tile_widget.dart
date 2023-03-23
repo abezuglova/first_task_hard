bced1e81678cecc176b0e8c3dcd3c47d657a7357
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-03-23 16:28:59 +0300

    two animations added

--- lib/widgets/level_tile_widget.dart
+++ lib/widgets/level_tile_widget.dart
@@ -3,45 +3,76 @@ import 'package:first_task_hard/widgets/tile_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LevelTileWidget extends StatelessWidget {
+class LevelTileWidget extends StatefulWidget {
   const LevelTileWidget({super.key});
+
+  @override
+  State<LevelTileWidget> createState() => _LevelTileWidgetState();
+}
+
+class _LevelTileWidgetState extends State<LevelTileWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 1),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInOut,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return TileWidget(
-      rotateAngle: 4.58,
-      tileWidth: 301.66.w,
-      tileHeight: 122.h,
-      child: Column(
-        children: [
-          SizedBox(height: 16.h),
-          Text(
-            'Уровень достижений: 5',
-            style: textTheme.headlineSmall,
-          ),
-          SizedBox(height: 30.h),
-          Stack(
+    return ScaleTransition(
+      scale: _animation,
+      child: GestureDetector(
+        onTap: () {
+          _controller.isAnimating
+              ? _controller.stop()
+              : _controller.repeat(reverse: true);
+        },
+        child: TileWidget(
+          rotateAngle: 4.58,
+          tileWidth: 301.66.w,
+          tileHeight: 122.h,
+          child: Column(
             children: [
-              Container(
-                width: 220.81.w,
-                height: 31.38.h,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color: AppColors.buttonColor,
-                ),
+              SizedBox(height: 16.h),
+              Text(
+                'Уровень достижений: 5',
+                style: textTheme.headlineSmall,
               ),
-              Container(
-                width: 159.72.w,
-                height: 31.38.h,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color: AppColors.progressColor,
-                ),
+              SizedBox(height: 30.h),
+              Stack(
+                children: [
+                  Container(
+                    width: 220.81.w,
+                    height: 31.38.h,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color: AppColors.buttonColor,
+                    ),
+                  ),
+                  Container(
+                    width: 159.72.w,
+                    height: 31.38.h,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color: AppColors.progressColor,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/widgets/profile_tile_widget.dart
+++ lib/widgets/profile_tile_widget.dart
@@ -1,26 +1,55 @@
 import 'package:first_task_hard/widgets/tile_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:math' as math;
 
-class ProfileTileWidget extends StatelessWidget {
+class ProfileTileWidget extends StatefulWidget {
   const ProfileTileWidget({super.key});
+
+  @override
+  State<ProfileTileWidget> createState() => _ProfileTileWidgetState();
+}
+
+class _ProfileTileWidgetState extends State<ProfileTileWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 10),
+    vsync: this,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return GestureDetector(
-      onTap: () {},
-      child: TileWidget(
-        rotateAngle: 27.76,
-        tileWidth: 112.92.w,
-        tileHeight: 89.56.h,
-        child: Center(
-          child: Text(
-            'Профиль',
-            style: textTheme.headlineSmall,
+    return AnimatedBuilder(
+      animation: _controller,
+      child: GestureDetector(
+        onTap: () {
+          _controller.isAnimating ? _controller.stop() : _controller.repeat();
+        },
+        child: TileWidget(
+          rotateAngle: 27.76,
+          tileWidth: 112.92.w,
+          tileHeight: 89.56.h,
+          child: Center(
+            child: Text(
+              'Профиль',
+              style: textTheme.headlineSmall,
+            ),
           ),
         ),
       ),
+      builder: (BuildContext context, Widget? child) {
+        return Transform.rotate(
+          angle: _controller.value * 2.0 * math.pi,
+          child: child,
+        );
+      },
     );
   }
 }
