e8bddd0d22c7471515030f3ee7a0e759e92782c7
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-03-23 10:42:28 +0300

    app colors added

---
+++ lib/assets/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const mainTextColor = Color(0x0FF3314B);
+  static const buttonTextColor = Color(0xFF4D5DFA);
+  static const redTextColor = Color(0xFFE6492D);
+  static const buttonColor = Color(0xFFEDEFFF);
+  static const progressColor = Color(0xFFAAFF82);
+}
