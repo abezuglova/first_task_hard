335392d9cc4d52d055d8fe9956e4cea94bded493
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-03-23 12:23:21 +0300

    my shifts tile widget created

--- lib/widgets/my_shifts_tile_widget.dart
+++ lib/widgets/my_shifts_tile_widget.dart
@@ -1,11 +1,16 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:first_task_hard/widgets/tile_widget.dart';
+import 'package:flutter/material.dart';
 
 class MyShiftsTileWidget extends StatelessWidget {
   const MyShiftsTileWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const TileWidget(
+      rotateAngle: -2.32,
+      tileWidth: 175.49,
+      tileHeight: 73.81,
+      child: Text('Мои смены'),
+    );
   }
 }
